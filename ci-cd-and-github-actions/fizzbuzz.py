13912a2e7b7ef0f513e6e6ee84c5ed101f590940
su-sid <davidsudi20@gmail.com>
2025-03-20 22:20:10 +0300

    testing the updated test file

--- ci-cd-and-github-actions/fizzbuzz.py
+++ ci-cd-and-github-actions/fizzbuzz.py
@@ -11,8 +11,10 @@ def fizzbuzz(n):
         if i % 15 == 0:
             result.append("FizzBuzz")
         elif i % 3 == 0:
+            
             result.append("Fizz")
         elif i % 5 == 0:
+            
             result.append("Buzz")
         else:
             result.append(i)
