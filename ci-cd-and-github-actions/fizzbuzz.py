b01a877f643d3805708b8c661abe4336fbf6f951
su-sid <davidsudi20@gmail.com>
2025-03-20 22:24:46 +0300

    update

--- ci-cd-and-github-actions/fizzbuzz.py
+++ ci-cd-and-github-actions/fizzbuzz.py
@@ -11,10 +11,8 @@ def fizzbuzz(n):
         if i % 15 == 0:
             result.append("FizzBuzz")
         elif i % 3 == 0:
-            
             result.append("Fizz")
         elif i % 5 == 0:
-            
             result.append("Buzz")
         else:
             result.append(i)
